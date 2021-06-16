ef5252a532a14d62e44a26ada51c06c03fb9b5de
Nadia Iradatul Muhsinah <72561485+Nadia-Iradatul-Muhsinah@users.noreply.github.com>
2021-06-16 11:36:29 +0700

    Nadia_PWEBA_Kel10

---
+++ index_admin.php
@@ -0,0 +1,42 @@
+<?php
+
+require "header2.php";
+require "header_admin.php";
+
+if(!$_SESSION['is_login']){
+        header('location:login.php');?>
+        <a href="login.php"></a>
+        <?php
+    }
+?>
+<br><br><br>
+
+<?php 
+// require "header2.php";
+// session_start();
+if(! isset($_SESSION['is_login']))
+{
+  header('location:login.php');
+}
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>HOME</title>
+    <style>
+    .qwe{
+        background: url(1.webp);
+        background-size: cover;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="qwe">
+    <br>
+        <center><h1>Selamat Datang <?php echo $_SESSION['nama']; ?></h1></center>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </div>
+</body>
+</html>
